fb67513ca655bd7c5acc492dadd339606cc934b2
Khadjimuratov Ulugbek <khadjimuratovulugbek@gmail.com>
2022-11-30 19:44:01 +0000

    This is done whole project

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -9,3 +9,28 @@ cpu_score = 0
 player_score = 0
 
 print("To finish the game please write 'end'")
+
+while True:
+    player = input("Rock,Paper or Scissors?").capitalize()
+    #Conditions of Rock,Paper amd Scissors
+    if player == computer:
+        print("Tie")
+    elif player == "Rock":
+        if computer == "Paper":
+           print("You lose!", computer, "covers", player)
+           cpu_score+=1
+        else:
+            print("You win!", player, "smashes", computer)
+            player_score+=1
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+            cpu_score+=1
+        else:
+            print("You win", player, "covers", computer)
+            player_score+=1
+    elif player=='End':
+        print("Final scores:")
+        print(f"CPU:{cpu_score}")
+        print(f"Player:{player_score}")
+        break
