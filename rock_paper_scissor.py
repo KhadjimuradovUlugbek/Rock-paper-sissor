a73b84703c655c59bf399b99d33a8216a8d1217d
Khadjimuratov Ulugbek <khadjimuratovulugbek@gmail.com>
2022-11-30 19:25:53 +0000

    This is the half project

---
+++ rock_paper_scissor.py
@@ -0,0 +1,11 @@
+import random
+choices = ["Rock", "Paper", "Scissors"]
+
+computer = random.choice(choices)
+
+player = False
+
+cpu_score = 0
+player_score = 0
+
+print("To finish the game please write 'end'")
